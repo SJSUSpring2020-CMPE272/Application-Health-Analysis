69df5099164bad53b5e3b33a3b4798183511ee56
Vamsi-Mundra <60533855+Vamsi-Mundra@users.noreply.github.com>
2020-05-04 02:43:14 -0700

    handling failure alert and sending mail

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+// app.use(cors({ origin: '*', credentials: true}));
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const logger = require('tracer').colorConsole();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', true);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   res.setHeader('Cache-Control', 'no-cache');
