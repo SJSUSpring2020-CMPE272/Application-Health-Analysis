7807d296893c8b8eeb84e8439b3dd9502ce5a138
Akhil Reddy <akhilreddymandadi@gmail.com>
2020-05-05 01:20:00 -0700

    Index

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: process.env.REACT_URL, credentials: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const logger = require('tracer').colorConsole();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', true);
+  res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
